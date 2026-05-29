01e4a7ab2fc6e682986f03143f29f236c3fc9f98
Gabriel-Fernandes27 <gabriel123gabriel2020@gmail.com>
2026-05-29 15:19:20 -0300

    Metodo Autalizar + Excluir

---
+++ Repository/UsuarioRepository.cs
@@ -0,0 +1,80 @@
+﻿using MySql.Data.MySqlClient;
+using Segundo_App_BancoDados.Models;
+using Segundo_App_BancoDados.Repository.Contract;
+using System.Data;
+
+namespace Segundo_App_BancoDados.Repository
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly string _conexaoMySQL;
+
+        public UsuarioRepository(IConfiguration conf)
+        {
+           _conexaoMySQL = conf.GetConnectionString("ConexaoMySQL");
+        }
+        public void Atualizar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Usuario usuario)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("insert into usuario(nomeUsu, Cargo, DataNasc) " +
+                                                " values (@nomeUsu, @Cargo, @DataNasc )", conexao); // @: PARAMETRO
+
+                cmd.Parameters.Add("@nomeUsu", MySqlDbType.VarChar).Value = usuario.nomeUsu;
+                cmd.Parameters.Add("@Cargo", MySqlDbType.VarChar).Value = usuario.cargo;
+                // cmd.Parameters.Add("@DataNasc", MySqlDbType.VarChar).Value = usuario.DataNasc.ToString("yyyy/MM/dd");
+                cmd.Parameters.Add("@DataNasc", MySqlDbType.VarChar).Value = usuario.dataNasc.ToString("yyyy/MM/dd");
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Usuario> ObterTodosUsuarios()
+        {
+            List<Usuario> UsuarioList = new List<Usuario>();
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("Select * from usuario", conexao);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                conexao.Clone();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    UsuarioList.Add(
+                        new Usuario
+                        {
+                            IdUsu = Convert.ToInt32(dr["IdUsu"]),
+                            nomeUsu = (string)dr["nomeUsu"],
+                            cargo = (string)dr["Cargo"],
+                            dataNasc = Convert.ToDateTime(dr["DataNasc"])
+
+                        });
+                }
+                return UsuarioList;
+            }
+        }
+
+        public Usuario ObterUsuario(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
